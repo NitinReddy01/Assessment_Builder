be27c5adb453998ed011c5218a84bb6d3beb02a7
NitinReddy01 <nitingogula@gmail.com>
2024-06-11 10:50:09 +0530

    added audio question

---
+++ server/src/Assessment_Builder/AudioQuestionItem.ts
@@ -0,0 +1,23 @@
+import AudioQuestion from "../models/AudioQuestion";
+import { QuesitonType } from "../models/FIB_Schema";
+
+export class AudioQuestionItem{
+    private question:QuesitonType[];
+    private type:string;
+    private time:string;
+
+    constructor(question:QuesitonType[],type:string,time:string){
+        this.question = question;
+        this.type = type;
+        this.time = time;
+    }
+
+    async create(){
+        const audioQuestion = await AudioQuestion.create({
+            question:this.question,
+            type:this.type,
+            time:this.time,
+        })
+        return String(audioQuestion._id);
+    }
+}
