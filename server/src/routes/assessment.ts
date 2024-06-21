1c2d8b8368946524df1a3ffcf5e2fb756bfdf8cc
Nikhil <karthikagastya2002@gmail.com>
2024-06-21 13:10:24 +0530

    updated add assessment

--- server/src/routes/assessment.ts
+++ server/src/routes/assessment.ts
@@ -1,110 +1,13 @@
 import { Router } from "express";
-import { AssessmentBuilder, AssessmentParts } from "../Assessment_Builder/AssessmentBuilder";
-import Assessment, { IAsessment } from "../models/Assessment_Schema";
-import MCQ, { IMCQ } from "../models/MCQ_Schema";
-import FIB, { IFIB, QuesitonType } from "../models/FIB_Schema";
-import MTF, { IMTF } from "../models/MTF_Schema"
-import AudioQuestion, { IAudioQuestion } from "../models/AudioQuestion";
-import mongoose from "mongoose";
-import Template from "../models/Template_Schema";
-import Parts from "../models/TemplateParts_Schema";
+import { AssessmentBuilder, AssessmentTemplate, } from "../Assessment_Builder/AssessmentBuilder";
+import Assessment from "../models/Assessment_Schema";
+import Parts from "../models/TemplateParts_Schema"
+import MCQ from "../models/MCQ_Schema";
+import FIB from "../models/FIB_Schema";
+import MTF from "../models/MTF_Schema"
+import AudioQuestion from "../models/AudioQuestion";
+
 const assessmentRouter = Router();
-
-interface IAssessmentWithId extends IAsessment {
-    _id: mongoose.Types.ObjectId;
-}
-// TODO: need to do input validaton for all these routes 
-
-interface CreateParts {
-    name: string;
-    instruction: string;
-    description: string;
-    time: string;
-    content: QuesitonType[];
-    policies: {
-      grade: {
-        questionType: string;
-        weightage: number;
-      }[];
-    };
-    items: {
-      questionType: string;
-      tag: string;
-      question: QuesitonType[];
-      fibAnswers?: QuesitonType[];
-      options?: {
-        contentType: string;
-        key: {
-          value: string;
-          isAnswer: boolean;
-        };
-      }[];
-      leftOptions?: QuesitonType[];
-      rightOptions?: QuesitonType[];
-      mtfAnswers?: {
-        leftAnswer?: QuesitonType;
-        rightAnswer?: QuesitonType;
-      }[];
-    }[];
-  }
-
-  const transformParts = (createParts: CreateParts[]): AssessmentParts[] => {
-    return createParts.map(part => {
-        const transformedItems = part.items.map(item => {
-            switch (item.questionType) {
-                case "MCQ":
-                    return {
-                        questionType: item.questionType,
-                        question: item.question,
-                        options: item.options?.map(option => ({contentType:option.contentType,key:option.key.value})) || [],
-                        answers: item.options?.filter(option => option.key.isAnswer).map(option => ({key:option.key.value,contentType:option.contentType})) || [],
-                        time: part.time,
-                        tag: item.tag
-                    } as unknown as IMCQ;
-                case "FIB":
-                    return {
-                        questionType: item.questionType,
-                        question: item.question,
-                        answers: item.fibAnswers || [],
-                        time: part.time,
-                        tag: item.tag
-                    } as IFIB;
-                case "MTF":
-                    return {
-                        questionType: item.questionType,
-                        question: item.question,
-                        leftOptions: item.leftOptions || [],
-                        rightOptions: item.rightOptions || [],
-                        answers: item.mtfAnswers?.map(answer => ({
-                            leftAnswer: answer.leftAnswer,
-                            rightAnswers: [answer.rightAnswer]
-                        })) || [],
-                        time: part.time,
-                        tag: item.tag
-                    } as unknown as IMTF;
-                case "AudioQuestion" :
-                    return {questionType:item.questionType,question:item.question,time:part.time} as IAudioQuestion;
-                default:
-                    throw new Error(`Unsupported question type: ${item.questionType}`);
-            }
-        });
-
-        return {
-            name: part.name,
-            instruction: part.instruction,
-            description: part.description,
-            time: part.time,
-            content: part.content,
-            policies: {
-                grade: part.policies.grade.map(policy => ({
-                    questionType: policy.questionType,
-                    weightage: policy.weightage
-                }))
-            },
-            items: transformedItems
-        } as unknown as AssessmentParts;
-    });
-};
 
 assessmentRouter.post('/add-assessment', async (req, res) => {
     try {
@@ -118,7 +21,7 @@ assessmentRouter.post('/add-assessment', async (req, res) => {
         const assessment:AssessmentTemplate  = req.body.assessment;
         const time = req.body.time;
         const newAssessment = new AssessmentBuilder();
-        const id = await newAssessment.createAssessment(title, assessment, type, time);
+        const id = await newAssessment.createAssessment(title, assessment, type, time,bucket,month,week,Class);
         res.status(201).json({ message: "Assessment Created", id });
     } catch (error) {
         console.log(error);
@@ -146,6 +49,7 @@ assessmentRouter.get('/assessment/:id', async (req, res) => {
             return res.status(404).json({ message: "Assessment not found" });
         }
 
+        // Function to populate items based on question type
         const populateItems = async (parts: any[]) => {
             for (const part of parts) {
                 for (const item of part.items) {
@@ -177,61 +81,6 @@ assessmentRouter.get('/assessment/:id', async (req, res) => {
 });
 
 
-export const deleteParts = async (parts:mongoose.Schema.Types.ObjectId[],session:mongoose.mongo.ClientSession)=>{
-    try {
-        const allParts = await Parts.find({ _id: { $in: parts } });
-        for (const part of allParts) {
-            const questionIds = part.items.map(item => item.questionId);
-            await AudioQuestion.deleteMany({ _id: { $in: questionIds } }).session(session);
-            await FIB.deleteMany({ _id: { $in: questionIds } }).session(session);
-            await MCQ.deleteMany({ _id: { $in: questionIds } }).session(session);
-            await MTF.deleteMany({ _id: { $in: questionIds } }).session(session);
-        }
-        await Parts.deleteMany({ _id: { $in: parts } }).session(session);
-    } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
-        throw error;
-    }
-}
-
-const deleteAssesssment = async (assessment:IAssessmentWithId) => {
-    
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    
-    try {
-        await deleteParts(assessment.parts,session);
-        await Assessment.findByIdAndDelete(assessment._id).session(session);
-        await session.commitTransaction();
-        session.endSession();
-    } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
-        throw error;
-    }
-};
-
-assessmentRouter.delete('/assessment/:id', async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ error: 'Invalid template ID' });
-    }
-    const assessment = await Assessment.findById(id);
-    if (!assessment) {
-        return res.status(200).json({message:"Assessment not found"});
-    };
-    try {
-        // await deleteAssesssment(new mongoose.Types.ObjectId(id));
-        await deleteAssesssment(assessment);
-        res.status(200).json({ message: 'Assessment and all associated data deleted successfully' });
-        // res.status(200).json(assessment);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error deleting template' });
-    }
-});
 
 
 export default assessmentRouter;
